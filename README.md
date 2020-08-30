07e2fb99da3c1d8052a12df92bde3a9b2ab78960
Alex Nevpryaga <alexnevpryaga@cloud.upwork.com>
2020-08-30 14:59:22 +0300

    rewrite code using TrinketHidCombo lib for both mouse and kbd support

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 The simple activity emulator on Digispark usb board =)
 In loop changes chrome and phpstorm windows (and tabs in it) and preses 'CTRL' key many times.
 
+ - mouse and keyboard support; 
+ - TrinketHidCombo lib is used (https://github.com/adafruit/Adafruit-Trinket-USB)
+
 **for now it works only on mac/linux**
 
 Pins 0 1 2 can be used for changing mode:
  - w/o jumper or closed 0-1: soft mode. just ctrls with random delay.
  - pins 1-2: full mode. randomly changing applications and its windows (phpstorm,chrome and slack supported for now) and scroll content and random ctrls as well.
+
+
 
 # Board
 ![Board](https://s3.amazonaws.com/digistump-resources/images/l/2520dcf84a0a3bf2257c2559d7a187db.jpg)
--- arduino_sae.ino
+++ arduino_sae.ino
@@ -1,3 +1,5 @@
+#include "TrinketHidCombo.h"
+
 /**
  * @author Alex Nevpryaga
  * @source https://github.com/santaklouse/arduino_sae
@@ -7,12 +9,15 @@
  * just for fun...
  * 
 
-  You can also change vendor of device 
-  path ~/Library/Arduino15/packages/digistump/hardware/avr/1.6.7/libraries/DigisparkKeyboard/usbconfig.h
+  ...using TrinketHidCombo lib from https://github.com/adafruit/Adafruit-Trinket-USB
+
+  You can change vendor of device here:
+  <Arduino path>/libraries/TrinketHidCombo/usbconfig.h
 
   Below there are values for Apple Keyboard
   #define USB_CFG_VENDOR_ID 0xac, 0x05
   #define USB_CFG_DEVICE_ID 0x02, 0x02
+  
   #define USB_CFG_VENDOR_NAME     'A','p','p','l','e',' ','I','n','c','.'
   #define USB_CFG_VENDOR_NAME_LEN 10
   #define USB_CFG_DEVICE_NAME     'K','e','y','b','o','a','r','d'
@@ -21,7 +26,7 @@
  * 
  * for now it works only on mac and linux
  */
-#include "DigiKeyboard.h"
+
 
 #define LED_PIN 1
 
@@ -49,21 +54,77 @@
 #define WIN_OS 1
 #define LINUX_OS 2
 
-#define KEY_DELETE 42
-#define KEY_TAB 43
-#define KEY_PAGE_UP 0x4B
-#define KEY_PAGE_DOWN 0x4E
-#define KEY_LEFT_CTRL 0xe0
-
 
 /*
- * existing key codes: https://github.com/digistump/DigisparkArduinoIntegration/blob/master/libraries/DigisparkKeyboard/DigiKeyboard.h
+ * more codes can be found here: https://raw.githubusercontent.com/adafruit/Adafruit-Trinket-USB/master/TrinketHidCombo/TrinketHidCombo.h
  * additional key codes: https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
  */
 
+// keycodes
+#define KEY_A 0x04
+#define KEY_ENTER 0x28
+#define KEY_DELETE 0x4C
+#define KEY_SPACE 0x2C
+#define KEY_TAB 0x2B
+#define KEY_PAGE_UP 0x4B
+#define KEY_PAGE_DOWN 0x4E
+#define KEY_LEFT_CTRL 0xE0
+#define KEY_LEFT_ALT 0xE2
+#define KEY_LEFT_GUI 0xE3
+#define KEY_RIGHT_CONTROL 0xE4
+
+// modifier keys
+#define MOD_GUI_LEFT 0x08
+#define MOD_CONTROL_LEFT 0x01
+#define MOD_ALT_LEFT 0x04
+
+void blink() {
+  LED_LIGHT_ON();
+  delay(300);
+  LED_LIGHT_OFF();
+  delay(100);
+}
+
 void log(String str, bool r = false) {
-  DigiKeyboard.print(str);
-  if (r) DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  TrinketHidCombo.print(str);
+  if (r) pressKey(KEY_ENTER);
+}
+
+void pressKey(uint8_t modifiers, uint8_t keycode1) {
+  TrinketHidCombo.pressKey(modifiers, keycode1);
+  TrinketHidCombo.pressKey(0, 0);
+  blink();
+}
+
+void pressKey(uint8_t keycode1) {
+  pressKey(0, keycode1);
+}
+
+void mouseMove(signed char x, signed char y, uint8_t buttonMask) {
+  TrinketHidCombo.mouseMove(x, y, buttonMask);
+  TrinketHidCombo.mouseMove(0, 0, 0);
+}
+
+void mouseMove(signed char x, signed char y) {
+  mouseMove(x, y, 0);
+}
+
+void mouseMove(uint8_t buttonMask) {
+  mouseMove(0, 0, buttonMask);
+}
+
+void mouseLeftClick() {
+  mouseMove(MOUSEBTN_LEFT_MASK);
+}
+
+void mouseMiddleClick() {
+  mouseMove(MOUSEBTN_MIDDLE_MASK);
+}
+
+void moveMouseToLeftBottom() {
+  for (int i = 0; i < 20; i++) {
+    mouseMove(-127, 127);
+  }
 }
 
 void setPinToPullupMode(byte pinNumber) {
@@ -73,14 +134,6 @@ void setPinToPullupMode(byte pinNumber) {
 
 bool isPinShorted(byte pin) {
   return digitalRead(pin) == LOW;
-}
-
-void setup() {
-  setPinToPullupMode(PIN_LIGHT_MODE_NUM);
-  pinMode(LED_PIN, OUTPUT);
-  setPinToPullupMode(PIN_FULL_MODE_NUM);
-
-  randomSeed(analogRead(0));
 }
 
 byte mode = -1;
@@ -121,37 +174,37 @@ void openSearch() {
   delay(random(100, 200));
   switch(detectPlatform()) {
     case MAC_OS:
-      DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT); // Super key, open 'search'
+      pressKey(MOD_GUI_LEFT, KEY_SPACE); // Super key, open 'search'
       break;
     case LINUX_OS:
     case WIN_OS:
-      DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT); // Super key, open 'search'
+      pressKey(KEY_LEFT_GUI); // Super key, open 'search'
       break;
   }
   
   delay(random(300, 400));
-  DigiKeyboard.sendKeyStroke(KEY_A, MOD_CONTROL_LEFT);
-  DigiKeyboard.sendKeyStroke(KEY_DELETE); //clear
+  pressKey(MOD_CONTROL_LEFT, KEY_A);
+  pressKey(KEY_DELETE); //clear
   delay(random(300, 400));
 }
 
 void openByCommand(char* cmd) {
   openSearch();
   delay(random(100, 300));
-  DigiKeyboard.print(cmd);
+  TrinketHidCombo.print(cmd);
   delay(random(200, 400));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  pressKey(KEY_ENTER);
 }
 
 void changeTab() {
-  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_CONTROL_LEFT);
+  pressKey(MOD_CONTROL_LEFT, KEY_TAB);
 }
 
 void scroll() {
   if (random(0,1)) {
-    DigiKeyboard.sendKeyStroke(KEY_PAGE_DOWN);
+    pressKey(KEY_PAGE_DOWN);
   } else {
-    DigiKeyboard.sendKeyStroke(KEY_PAGE_UP);
+    pressKey(KEY_PAGE_UP);
   }
   delay(random(1000, 5000));
 }
@@ -161,12 +214,14 @@ void openChrome() {
   ctrls();
   delay(random(500, 3000));
   changeTab();
+  moveMouseToLeftBottom();
+  mouseLeftClick();
   scroll();
 }
 
 void openPhpStorm() {
   openByCommand("phpstorm");
-  ctrls();
+  scroll();
   delay(random(500, 3000));
   changeTab();
   scroll();
@@ -184,7 +239,6 @@ void openDesktop() {
   //TODO
   blinkRandomly();
   ctrls();
-//  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
 }
 
 void blinkRandomly() {
@@ -194,25 +248,16 @@ void blinkRandomly() {
   delay(random(10, 1000));
 }
 
-void alts() {
-  for (int i = 0; i < 10; i++) {
-    LED_LIGHT_ON();
-    DigiKeyboard.sendKeyStroke(0, MOD_ALT_LEFT);
-    delay(50);
-    LED_LIGHT_OFF();
-    delay(random(50, 100));
-  }
-}
-
 void ctrls() {
-  for (int i = 0; i < 10; i++) {
+  int amount = random(1, 10);
+  for (int i = 0; i < amount; i++) {
     LED_LIGHT_ON();
     //use magic Ctrl's combination in order to avoid side-effects
-    DigiKeyboard.sendKeyStroke(KEY_LEFT_CTRL, MOD_CONTROL_LEFT);
-    DigiKeyboard.delay(100);
+    pressKey(MOD_CONTROL_LEFT, KEY_LEFT_CTRL);
+    delay(100);
     LED_LIGHT_OFF();
     //random delay between 1s and 10s
-    DigiKeyboard.delay(random(1110, 10201));
+    delay(random(1110, 10201));
   }
 }
 
@@ -221,29 +266,24 @@ void runLightCycle() {
   LED_LIGHT_ON();
 
   //use magic Ctrl's combination (MOD_CTRL+CTRL) in order to avoid side-effects
-  DigiKeyboard.sendKeyStroke(KEY_LEFT_CTRL, MOD_CONTROL_LEFT);
-
-  DigiKeyboard.delay(150);
+  pressKey(MOD_CONTROL_LEFT, KEY_LEFT_CTRL);
+  
+  delay(150);
 
   //turn light off
   LED_LIGHT_OFF();
 
   //random delay between 7s and 20s
-  DigiKeyboard.delay(random(7110, 20201));
+  delay(random(7110, 20201));
 }
 
 void runFullCycle() {
-  LED_LIGHT_ON();
-  DigiKeyboard.delay(300);
-  LED_LIGHT_OFF();
-  DigiKeyboard.delay(100);
-  LED_LIGHT_ON();
-  DigiKeyboard.delay(300);
-  LED_LIGHT_OFF();
-
+  moveMouseToLeftBottom();
+  mouseMiddleClick();
+  
   // this is generally not necessary but with some older systems it seems to
   // prevent missing the first character after a delay:
-  DigiKeyboard.sendKeyStroke(0);
+  TrinketHidCombo.pressKey(0, 0);
   
   ctrls();
 
@@ -267,16 +307,40 @@ void runFullCycle() {
   }
   delay(random(2000, 5000));
 }
+byte workMode = 0;
+void setup() {
+  setPinToPullupMode(PIN_LIGHT_MODE_NUM);
+  pinMode(LED_PIN, OUTPUT);
+  setPinToPullupMode(PIN_FULL_MODE_NUM);
+
+  TrinketHidCombo.begin(); // start the USB device engine and enumerate
+
+  randomSeed(analogRead(0));
+
+  workMode = detectMode();
+  if (workMode == MODE_LIGHT) {
+    blink();
+    blink();
+  } else if (workMode == MODE_FULL) {
+    blink();
+    blink();
+    blink();
+  } else {
+    blink();
+    blink();
+  }
+}
 
 void loop() {
-  byte mode = detectMode();
-
-  if (mode == MODE_LIGHT) {
+   
+  workMode = detectMode();
+  if (workMode == MODE_LIGHT) {
       runLightCycle();
-    } else if (mode == MODE_FULL) {
+  } else if (workMode == MODE_FULL) {
       runFullCycle();
-    } else {
-      //same as light for now
-      runLightCycle();
-    }
+  } else {
+    //same as light for now
+    runLightCycle();
+  }
+  TrinketHidCombo.poll(); // check if USB needs anything done
 }
